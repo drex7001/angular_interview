ab6575dc95b2347c3166cf364dba1c040a40cc11
drex7001 <ayodhyaratnayake7001@gmail.com>
2021-12-10 15:49:01 +0530

    initial commit

---
+++ Tache/Test-Laravel/test-laravel-app/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/login', function () {
+//     return view('login');
+// });
+
+// Route::get('/register', function () {
+//     return view('register');
+// });
+
+Route::get('/login',[PageController::class, 'indexLogin']);
+
+Route::get('/register',[PageController::class, 'indexRegister']);
